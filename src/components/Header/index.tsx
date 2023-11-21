7ca21272e10645657de0da2c292a88b3d64133cb
LUCAS AREND MACIEL <Lucaskmaciel@hotmail.com>
2023-11-21 14:20:51 -0300

    make a pomodoro clock

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import { HeaderContiner } from './styles'
+
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { Timer, Scroll } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+
+export function Header() {
+  return (
+    <HeaderContiner>
+      <img src={logoIgnite} alt="" />
+      <nav>
+        <NavLink to="/">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContiner>
+  )
+}
